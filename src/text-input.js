3233e15dfedc348bffbae5cbb98adc7f501f5d7b
Wakana <wakaichi@gmail.com>
2023-03-27 16:16:37 +0400

    created text input and range input

---
+++ src/text-input.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function TextInput(){
+    const [value, setValue] = useState('');
+  
+    function handleChange(event){
+      setValue(event.target.value)
+    }
+  
+    return (
+      
+      <div>
+      <input
+      className='textInput'
+        type="text"
+        id="textInput"
+        name="textInput"
+        placeholder="P4$5W0rD!"
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+    );
+  }
+  export default TextInput;
