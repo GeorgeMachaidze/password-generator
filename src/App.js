8f562ec5a0143c37b7109c7496580a17cad0f619
Wakana <wakaichi@gmail.com>
2023-03-30 18:11:27 +0400

    project finshed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import React, { useState,useRef } from "react";
+import React, { useState,useRef,useEffect } from "react";
 
 function App() {
   
@@ -11,9 +11,34 @@ function App() {
   const [numbers, setNumbers] = useState(false);
   const [symbols, setSymbols] = useState(false);
   const [title, setTitle] = useState('P4$5W0rD!');
+  const [color, setColor] = useState('rgba(129, 125, 146, 1)');
   const copyText = useRef(null);
   const [text, setText] = useState('')
+  const [backgroundColor1, setBackgroundColor1] = useState('rgba(24, 23, 31, 1)');
+  const [strengthText, setStrengthText] = useState('')
+  
+let allIsTrue = upperCase && lowerCase && numbers && symbols;
+let threeIsTrue = (!upperCase && lowerCase && numbers && symbols)  || (upperCase && !lowerCase && numbers && symbols)  || (upperCase && lowerCase && !numbers && symbols)  || (upperCase && lowerCase && numbers && !symbols);
+let twoIsTrue = (!upperCase && !lowerCase && numbers && symbols)  || (upperCase && lowerCase && !numbers && !symbols)  || (!upperCase && lowerCase && numbers && !symbols) || (upperCase && !lowerCase && !numbers && symbols) || (upperCase && !lowerCase && numbers && !symbols) || (!upperCase && lowerCase && !numbers && symbols);
+let oneIsTrue = (upperCase && !lowerCase && !numbers && !symbols) || (!upperCase && lowerCase && !numbers && !symbols) || (!upperCase && !lowerCase && numbers && !symbols) || (!upperCase && !lowerCase && !numbers && symbols);
 
+
+
+  function changeDivColor(){
+    if(allIsTrue){
+      setBackgroundColor1("rgba(164, 255, 175, 1)")
+      setStrengthText("STRONG")
+    }else if(threeIsTrue){
+      setBackgroundColor1("rgba(248, 205, 101, 1)")
+      setStrengthText("MEDIUM")
+    }else if(twoIsTrue){
+      setBackgroundColor1("rgba(251, 124, 88, 1)")
+      setStrengthText("WEAK")
+    }else if(oneIsTrue){
+      setBackgroundColor1("rgba(246, 74, 74, 1)")
+      setStrengthText("TOO WEAK")
+    }
+  }
   
 
   function handleCopy(){
@@ -51,10 +76,15 @@ function App() {
 
  
   function handleClick(){
-    const newPassword = generetePassword(upperCase, lowerCase, numbers, symbols, value);
     if(value === 0){
       setTitle("Please enter value")
+    }else{
+      const newPassword = generetePassword(upperCase, lowerCase, numbers, symbols, value);
+      changeDivColor();
+      setColor('#E6E5EA');
     }
+   
+    
   }
 
   function generetePassword(upperCase, lowerCase, numbers, symbols, value){
@@ -118,7 +148,7 @@ function App() {
       <h1>Password Generator</h1>
 
       <div className="passwordBox">
-        <p ref={copyText}>{title}</p>
+        <p style={{ color }} ref={copyText}>{title}</p>
         <div className="copy">
           <p>{text}</p>
         <svg onClick = {handleCopy}className="copyIcon" width="21"height="24"xmlns="http://www.w3.org/2000/svg"><path d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"fill="#A4FFAF"/>
@@ -195,12 +225,13 @@ function App() {
         <div className="secondSection">
           <p>STRENGTH</p>
           <div className="lvlDiv">
-            <p className="lvl"></p>
+            <p className="lvl">{strengthText}</p>
             <div className="rectangels">
-              <div className="firstRec rec"></div>
-              <div className="secondRec rec"></div>
-              <div className="thirdRec rec"></div>
-              <div className="fourthRec rec"></div>
+              <div className="firstRec rec"style={{ backgroundColor: allIsTrue || threeIsTrue || twoIsTrue || oneIsTrue ? backgroundColor1 : "" }}></div>
+              <div className="secondRec rec"style={{ backgroundColor: allIsTrue || threeIsTrue || twoIsTrue ? backgroundColor1 : ""  }}></div>
+              <div className="thirdRec rec"style={{ backgroundColor: allIsTrue || threeIsTrue ? backgroundColor1 : ""  }}></div>
+              <div className="fourthRec rec" style={{ backgroundColor: allIsTrue ? backgroundColor1 : "" }}></div>
+
             </div>
           </div>
         </div>
