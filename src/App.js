3f8a3510fb45c937b4e5805efdd432343e9d8ec0
Wakana <wakaichi@gmail.com>
2023-03-30 13:14:24 +0400

    add copy text and fixed minor bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 
 function App() {
   
@@ -10,6 +10,20 @@ function App() {
   const [lowerCase, setLowerCase] = useState(false);
   const [numbers, setNumbers] = useState(false);
   const [symbols, setSymbols] = useState(false);
+  const [title, setTitle] = useState('P4$5W0rD!');
+  const copyText = useRef(null);
+  const [text, setText] = useState('')
+
+  
+
+  function handleCopy(){
+    const text = copyText.current.textContent;
+    navigator.clipboard.writeText(text);
+    setText('COPIED')
+    setTimeout(() => {
+      setText('');
+    }, 2000);
+  }
 
   function handleChange(event) {
     setValue(event.target.value);
@@ -24,39 +38,79 @@ function App() {
   }
   function toggleUpperCase(){
     setUpperCase(!upperCase);
-    upperCaseLetter();
   }
   function toggleLowerCase(){
     setLowerCase(!lowerCase);
-    lowerCaseLetter();
   }
   function toggleNumbers(){
     setNumbers(!numbers);
-    getRandomNumber();
   }
   function toggleSymbols(){
     setSymbols(!symbols);
-    getRandomSymbol();
+  }
+
+ 
+  function handleClick(){
+    const newPassword = generetePassword(upperCase, lowerCase, numbers, symbols, value);
+    if(value === 0){
+      setTitle("Please enter value")
+    }
+  }
+
+  function generetePassword(upperCase, lowerCase, numbers, symbols, value){
+      let generatedPassword = "";
+      let variationsCount = 0;
+      if(upperCase){
+        variationsCount++;
+      }if(lowerCase){
+        variationsCount++;
+      }if(numbers){
+        variationsCount++;
+      }if(symbols){
+        variationsCount++;
+      }
+      if(!upperCase && !lowerCase && !numbers && !symbols){
+        for(let i = 0; i < value; i++)
+        generatedPassword += lowerCaseLetter();
+      }else{
+    for(let i = 0; i < value; i += variationsCount){
+      if(upperCase) {
+        generatedPassword += upperCaseLetter();
+      }
+      if(lowerCase) {
+        generatedPassword += lowerCaseLetter();
+      }
+      if(numbers) {
+        generatedPassword += getRandomNumber();
+      }
+      if(symbols) {
+        generatedPassword += getRandomSymbol();
+      }
+      
+    }
+  }
+    const finalPassword = generatedPassword.slice(0, value);
+    return setTitle(finalPassword);
   }
 
   function upperCaseLetter(){
     let charCode = Math.floor(Math.random() * (65 - 90 + 1)) + 90;
-    let UpperCaseLetter = String.fromCharCode(charCode);
-    return console.log(UpperCaseLetter);
+    let upperCaseLetter = String.fromCharCode(charCode);
+    return upperCaseLetter;
   }
   function lowerCaseLetter() {
     let charCode = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
     let lowerCaseLetter = String.fromCharCode(charCode);
-    return console.log(lowerCaseLetter);
+    return lowerCaseLetter;
   }
   function getRandomNumber(){
     let randomNumber = Math.floor(Math.random() * 10);
-    return console.log(randomNumber);
+    return randomNumber;
   }
   function getRandomSymbol() {
     const symbols = "!@#$%^&*(){}[]=<>/,.";
     let generetedSymbols = symbols[Math.floor(Math.random() * symbols.length)];
-    return console.log(generetedSymbols)
+    return generetedSymbols;
   }
 
   return (
@@ -64,9 +118,12 @@ function App() {
       <h1>Password Generator</h1>
 
       <div className="passwordBox">
-        <p>P4$5W0rD!</p>
-        <svg className="copyIcon" width="21"height="24"xmlns="http://www.w3.org/2000/svg"><path d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"fill="#A4FFAF"/>
+        <p ref={copyText}>{title}</p>
+        <div className="copy">
+          <p>{text}</p>
+        <svg onClick = {handleCopy}className="copyIcon" width="21"height="24"xmlns="http://www.w3.org/2000/svg"><path d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"fill="#A4FFAF"/>
         </svg>
+        </div>
       </div>
 
       <div className="mainBox">
@@ -148,7 +205,7 @@ function App() {
           </div>
         </div>
        
-          <button>
+          <button onClick={handleClick}>
             <p>GENERATE</p>
             <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg>
           </button>
